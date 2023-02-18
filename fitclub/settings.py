4de4e4a081d2614ad4265d4a7fc257b52f2d227b
LarisaLG <lgalisheva@gmail.com>
2023-02-18 18:41:01 +0000

    docs: add hero section and static files

--- fitclub/settings.py
+++ fitclub/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-i)e0v99=cz=4rwg(3!p0y7-a8^ky)unmu%7vj%77+g6()vx&21
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -150,6 +150,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
