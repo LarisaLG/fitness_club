c0b1be5430634d0909a71eddb929b997c53d989b
LarisaLG <lgalisheva@gmail.com>
2023-02-20 16:12:27 +0000

    docs: add page about template and view

--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ from django.shortcuts import render
 def index(request):
     """A view to return index page """
     return render(request, 'home/index.html')
+
+def about(request):
+    """
+    This view renders to the user the About page.
+    """
+    return render(request, 'home/about.html')
