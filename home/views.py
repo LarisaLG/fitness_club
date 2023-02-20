fad573cb7be77d65bdbd0167a98ff187486693be
LarisaLG <lgalisheva@gmail.com>
2023-02-20 20:48:37 +0000

    chore: add trainers sectio to aboit.html

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ def index(request):
     """A view to return index page """
     return render(request, 'home/index.html')
 
+
 def about(request):
     """
     This view renders to the user the About page.
