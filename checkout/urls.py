4d99ff247721e301a54e784c677bf2189127b999
LarisaLG <lgalisheva@gmail.com>
2023-02-25 19:24:08 +0000

    docs: fix urls.py and views  to checkout page, fix checkout.html ctructure

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('checkout/', views.checkout, name='checkout'),
+    path('checkout/<int:pack_id>', views.checkout, name='checkout'),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, reverse, redirect
-from memberships.models import Package
+from memberships.models import *
 
 # Create your views here.
 
 
-def checkout(request):
-    pack_details = PackDetail.objects.get()
+def checkout(request, pack_id):
+    package = Package.objects.get(id=pack_id)
+    pack_details = PackDetail.objects.all()
     context = {
+        'package': package,
         'pack': pack_details
     }
     return render(request, 'checkout/checkout.html', context)
