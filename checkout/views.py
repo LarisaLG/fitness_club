9aae6a42d822c8fa6c03a1f8f8fa748d4a4b9aa5
LarisaLG <lgalisheva@gmail.com>
2023-02-26 20:10:41 +0000

    chore: add checking if the user is logged in, add back button to package selection

--- checkout/views.py
+++ checkout/views.py
@@ -5,10 +5,17 @@ from memberships.models import *
 
 
 def checkout(request, pack_id):
-    package = Package.objects.get(id=pack_id)
-    pack_details = PackDetail.objects.all()
-    context = {
-        'package': package,
-        'pack': pack_details
-    }
-    return render(request, 'checkout/checkout.html', context)
+    """
+    This view renders to the user the checkout page or
+    redirects to login page if user not logged in.
+    """
+    if request.user.is_authenticated:
+        package = Package.objects.get(id=pack_id)
+        pack_details = PackDetail.objects.all()
+        context = {
+            'package': package,
+            'pack': pack_details
+        }
+        return render(request, 'checkout/checkout.html', context)
+    else:
+        return redirect('../accounts/login')
