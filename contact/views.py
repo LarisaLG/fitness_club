b41fa4f8e07b148914283dcc74d7d37900f928af
LarisaLG <lgalisheva@gmail.com>
2023-03-23 10:45:11 +0000

    docs: add functionality to send email

--- contact/views.py
+++ contact/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactUsForm
+from django.core.mail import send_mail
+from django.contrib import messages
 
 
+# Send e-mail
+# Code from CI lesson: Sending Real Emails with Django and
+# Code With Stein tutorial https://www.youtube.com/watch?v=dnhEnF7_RyM
 def contact(request):
     if request.method == 'POST':
         form = ContactUsForm(request.POST)
         if form.is_valid():
+            name = form.cleaned_data['name']
+            from_email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
             form.save()
+            send_mail(name, message, from_email, ['info@ponigym.com'])
+            messages.success(request,
+                             f'Your message successfully send.')
+            return redirect('index')
     else:
         form = ContactUsForm()
     return render(request, 'contact/contacts.html', {'form': form})
